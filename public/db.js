43d86e11973f89fab8f92321493e84ada1841e8b
Roomsie Jones <seangravito@gmail.com>
2021-08-17 17:35:41 -0500

    fixed service worker and manifest and db to match previous miniproject from nosql and from the pwa

--- public/db.js
+++ public/db.js
@@ -1,55 +1,136 @@
+// let db;
+// const request = indexedDB.open("budget", 1);
+
+// request.onupgradeneeded = function(event) {
+//   const db = event.target.result;
+//   db.createObjectStore("pending", { autoIncrement: true });
+// };
+
+// request.onsuccess = function(event) {
+//   db = event.target.result;
+// };
+
+// request.onerror = function(event) {
+//   console.log("Did not work! " + event.target.errorCode);
+// };
+
+// function saveRecord(record) {
+//     console.log('Saved');
+
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+
+//   store.add(record);
+// }
+
+// function checkDatabase() {
+//   const transaction = db.transaction(["pending"], "readwrite");
+//   const store = transaction.objectStore("pending");
+//   const getAll = store.getAll();
+
+//   console.log('getAll: ', getAll);
+
+//   getAll.onsuccess = function() {
+//     if (getAll.result.length > 0) {
+//       fetch("/api/transaction/bulk", {
+//         method: "POST",
+//         body: JSON.stringify(getAll.result),
+//         headers: {
+//           Accept: "application/json, text/plain, */*",
+//           "Content-Type": "application/json"
+//         }
+//       })
+//       .then(response => response.json())
+//       .then(() => {
+//         const transaction = db.transaction(["pending"], "readwrite");
+
+//         const store = transaction.objectStore("pending");
+
+//         store.clear();
+//       });
+//     }
+//   };
+// }
+
+// window.addEventListener("online", checkDatabase); 
+
+// using db.js from trhe noSQL miniproject
+
 let db;
-const request = indexedDB.open("budget", 1);
+let budgetVersion;
 
-request.onupgradeneeded = function(event) {
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
 };
 
-request.onsuccess = function(event) {
-  db = event.target.result;
+request.onerror = function (e) {
+  console.log(`Error! ${e.target.errorCode}`);
 };
-
-request.onerror = function(event) {
-  console.log("Did not work! " + event.target.errorCode);
-};
-
-function saveRecord(record) {
-    console.log('Saved');
-
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-
-  store.add(record);
-}
 
 function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
+  console.log('check db invoked');
+
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+
   const getAll = store.getAll();
 
-  console.log('getAll: ', getAll);
-
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
       })
-      .then(response => response.json())
-      .then(() => {
-        const transaction = db.transaction(["pending"], "readwrite");
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-        const store = transaction.objectStore("pending");
+            const currentStore = transaction.objectStore('BudgetStore');
 
-        store.clear();
-      });
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+          }
+        });
     }
   };
 }
 
-window.addEventListener("online", checkDatabase); 
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
+
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
+    checkDatabase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  const store = transaction.objectStore('BudgetStore');
+
+  store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,48 +1,147 @@
-const CACHE_NAME = 'static-cache-v1';
-
-const iconSizes = ['192', '512'];
-const iconFiles = iconsSizes.map((size) => `/icons/icon-${size}x${size}.png`);
-
 const FILES_TO_CACHE = [
-  "/",
-  "/offline.html",
-  '/manifest.webmanifest',
-  '/index.js',
-  '/styles.css'
-].concat(iconFiles);
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+  ];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches
-    .open(CACHE_NAME)
-    .then(cache => cache.addAll(FILE_TO_CACHE))
-    .then(() => self.skipWaiting())
-  );
-});
+  const STATIC_CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(
-        keyList.map(function(key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+//   self.addEventListener("install", function(evt) {
+//     evt.waitUntil(
+//       caches.open(CACHE_NAME).then(cache => {
+//         console.log("Your files were pre-cached successfully!");
+//         return cache.addAll(FILES_TO_CACHE);
+//       })
+//     );
+
+//     self.skipWaiting();
+//   });
+
+//   self.addEventListener("activate", function(evt) {
+//     evt.waitUntil(
+//       caches.keys().then(keyList => {
+//         return Promise.all(
+//           keyList.map(key => {
+//             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//               console.log("Removing old cache data", key);
+//               return caches.delete(key);
+//             }
+//           })
+//         );
+//       })
+//     );
+
+//     self.clients.claim();
+//   });
+
+//   self.addEventListener("fetch", function(evt) {
+//     if (evt.request.url.includes("/api/")) {
+//       evt.respondWith(
+//         caches.open(DATA_CACHE_NAME).then(cache => {
+//           return fetch(evt.request)
+//             .then(response => {
+//               if (response.status === 200) {
+//                 cache.put(evt.request.url, response.clone());
+//               }
+
+//               return response;
+//             })
+//             .catch(err => {
+//               return cache.match(evt.request);
+//             });
+//         }).catch(err => console.log(err))
+//       );
+
+//       return;
+//     }
+
+//     evt.respondWith(
+//          caches.match(evt.request).then(response => {
+//            console.log(response);
+//           return response || fetch(evt.request);
+//         })
+
+//     );
+//   });
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+        })
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+  
+    // handle runtime GET requests for data from /api routes
+    if (event.request.url.includes("/api")) {
+      // make network request and fallback to cache if network request fails (offline)
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
         })
       );
-    })
-    .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
+      return;
+    }
+  
+    // use cache first for all other requests for performance
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+  
+        // request is not in cache. make network request and cache the response
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  });
+  
